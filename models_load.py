06254e444dbd1ae244a55d58afa20ce7a4ed177c
bifu123 <415135222@qq.com>
2024-04-24 23:23:39 +0800

    debug

--- models_load.py
+++ models_load.py
@@ -151,53 +151,69 @@ async def chat_gemini(wxid, content, GMI_SERVER_URL):
 
 
 # 获取聊天记录
-def get_chat_history(data) -> list:
-    res = []
-    history_size_old = sys.getsizeof(f"{data}")
-    print("*" * 50)
-    print(f"以前聊天记录大小：{history_size_old}")
-    for item in data:
-        res.append({"query": item[0], "answer": item[1]})
-        print("*" * 50)
-        print(f"以前聊天记录：{res}")
-    return res
+def format_history(data):
+    formatted_data = ""
+    for entry in data:
+        formatted_data += f"query: {entry[0]}\n"
+        formatted_data += f"answer: {entry[1]}\n"
+    return formatted_data
 
 # 处理聊天记录
 async def do_chat_history(chat_history, source_id, query, answer, user_state, name_space):
-    # 插入当前数据表 source_id、query、result
-    insert_chat_history(source_id, query, answer, user_state, name_space)
-    # 将聊天记录入旧归档记录表history_old.xlsx表中
-    insert_chat_history_xlsx(source_id, query, answer, user_state)
-    # 当聊天记录数量超过2048时，删除数据库记录表history_now中时间最晚的两条记录
     history_size_now = sys.getsizeof(f"{chat_history}")
-    print("*" * 50)
-    print(f"当前聊天记录大小：{history_size_now}")
-    # 如果超过预定字节大小，就删除数据库中时间最旧的1条记录
-    if history_size_now > chat_history_size_set:
-        delete_oldest_records(source_id, user_state, name_space)
-        print("删除了数据库中时间最旧的1条记录")
+    # 如果超过预定字节大小就放弃写入
+    if not history_size_now > chat_history_size_set:
+        # 插入当前数据表 source_id、query、result
+        insert_chat_history(source_id, query, answer, user_state, name_space)
+        # 将聊天记录入旧归档记录表history_old.xlsx表中
+        insert_chat_history_xlsx(source_id, query, answer, user_state)
+    else:
+        print("记录过大，放弃写入")
 
 # 向量检索聊天（执行向量链）
 async def run_chain(retriever, source_id, query, user_state="聊天", name_space="test"):
     query = query.replace(f"{at_string} ", "")
     print("*" * 50)
     print("当前使用的知识库LLM：", llm_rag)
-    template_cn = """请根据上下文和对话历史记录完整地用中文回答问题，请用中文回答。Please answer in Chinese:
+    template_cn = """请根据上下文和对话历史记录用中文完整地回答问题 Please answer in Chinese:
     {context}
     {question}
     """
-    # 从数据库中提取source_id的聊天记录
-    data = fetch_chat_history(source_id, user_state, name_space)
-    chat_history = get_chat_history(data)
+    
 
+    # 处理聊天记录
+    data = fetch_chat_history(source_id, user_state, name_space) # 从数据库中提取source_id的聊天记录
+    chat_history = format_history(data)
+    
+    history_size_now = sys.getsizeof(f"{chat_history}") + sys.getsizeof(f"{query}") # 如果超过预定字节大小，删除记录
+    print("*" * 50)
+    print(f"预计聊天记录大小：{history_size_now}\n聊天记录：\n{chat_history}")
+    
+    while history_size_now > chat_history_size_set:
+        if history_size_now > chat_history_size_set:
+            delete_oldest_records(source_id, user_state, name_space) # 删除数据库中时间最旧的1条记录
+            if chat_history:
+                data.pop(0) # 删除chat_history中时间最旧的1条记录
+                chat_history = format_history(data)
+                history_size_now = sys.getsizeof(f"{chat_history}") + sys.getsizeof(f"{query}")
+                print("历史记录及问题字节之和超过预定值，删除时间最旧的1条记录")
+            else:
+                print("聊天记录为空，无需删除")
+                break
+        else:
+            break  # 如果条件不再满足，则跳出循环
+        
+        
     # 由模板生成prompt
     prompt = ChatPromptTemplate.from_template(template_cn) 
+    
     # 创建chain
     chain = RunnableMap({
         "context": lambda x: retriever.get_relevant_documents(x["question"]),
         "question": RunnablePassthrough(),
         "chat_history": lambda x: chat_history  # 使用历史记录的步骤
     }) | prompt | llm_rag | StrOutputParser()
+    
     # 执行问答
     request = {"question": query}
     try:
@@ -207,15 +223,38 @@ async def run_chain(retriever, source_id, query, user_state="聊天", name_space
     except Exception as e:
         response_message = "LLM响应错误"
         print(f"LLM响应错误: {e}")
+        
     # 返回结果
     return response_message
 
 # 通用聊天
 async def chat_generic_langchain(source_id, query, user_state="聊天",name_space="test"):
     query = query.replace(f"{at_string} ", "")
-    # 从数据库中提取 source_id 的聊天记录
-    data = fetch_chat_history(source_id, user_state, name_space)
-    chat_history = get_chat_history(data)
+    
+    
+    # 处理聊天记录
+    data = fetch_chat_history(source_id, user_state, name_space) # 从数据库中提取source_id的聊天记录
+    chat_history = format_history(data)
+    
+    history_size_now = sys.getsizeof(f"{chat_history}") + sys.getsizeof(f"{query}") # 如果超过预定字节大小，删除记录
+    print("*" * 50)
+    print(f"预计聊天记录大小：{history_size_now}\n聊天记录：\n{chat_history}")
+    
+    while history_size_now > chat_history_size_set:
+        if history_size_now > chat_history_size_set:
+            delete_oldest_records(source_id, user_state, name_space) # 删除数据库中时间最旧的1条记录
+            if chat_history:
+                data.pop(0) # 删除chat_history中时间最旧的1条记录
+                chat_history = format_history(data)
+                history_size_now = sys.getsizeof(f"{chat_history}") + sys.getsizeof(f"{query}")
+                print("历史记录及问题字节之和超过预定值，删除时间最旧的1条记录")
+            else:
+                print("聊天记录为空，无需删除")
+                break
+        else:
+            break  # 如果条件不再满足，则跳出循环
+
+  
     # 由模板生成 prompt
     prompt = ChatPromptTemplate.from_template("""
         你是一个热心的人，尽力为人们解答问题，请用中文回答。Please answer in Chinese:
@@ -223,6 +262,7 @@ async def chat_generic_langchain(source_id, query, user_state="聊天",name_spac
         {question}
     """)
     print("*" * 50)
+    
     # 创建链，将历史记录传递给链
     if user_state != "聊天" and must_use_llm_rag == True:
         chain = {
@@ -245,6 +285,7 @@ async def chat_generic_langchain(source_id, query, user_state="聊天",name_spac
     except Exception as e:
         response_message = "LLM响应错误"
         print(f"LLM响应错误: {e}")
+        
     return response_message
 
 
